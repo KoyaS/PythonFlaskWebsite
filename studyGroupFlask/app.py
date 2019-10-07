8e05e8f0104fc8655533255fa5740136d9eafc02
Koya <koyavsaito@gmail.com>
2019-10-07 15:52:50 -0400

    Finished Bare Bones
    Fixed issue where anonymous users could access dashboard without logging in, added sign out function.

--- studyGroupFlask/app.py
+++ studyGroupFlask/app.py
@@ -5,19 +5,28 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
+app.config['TESTING'] = False
 app.config['SECRET_KEY'] = 'secretString'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///Users/koya/Desktop/Projects/PythonFlaskWebsite/studyGroupFlask/database.db'
 Bootstrap(app)
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-
-class User(db.Model):
+class User(UserMixin, db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(15), unique=True)
 	email = db.Column(db.String(50), unique=True)
 	password = db.Column(db.String(80))
+
+@login_manager.user_loader
+def load_user(user_id):
+	return(User.query.get(int(user_id)))
 
 class LoginForm(FlaskForm):
 	username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
@@ -31,6 +40,7 @@ class RegisterForm(FlaskForm):
 	
 @app.route('/')
 def index():
+	logout_user()
 	return(render_template('index.html'))
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -42,8 +52,9 @@ def login():
 	 	
 	 	if existing_user: # Checking that user exists
 	 		
-	 		if existing_user.password == form.password.data:
-	 			return('<h1>Welcome {}<h1>'.format(form.username.data))
+	 		if check_password_hash(existing_user.password, form.password.data):
+	 			login_user(existing_user, remember=form.remember.data)
+	 			return(redirect(url_for('dashboard')))
 	 	
 	 	return('<h1> incorrect username or password</h1>')
 		# return('<h1>' + form.username.data + ' ' + form.password.data + '</h1>')
@@ -55,7 +66,8 @@ def signup():
 	form = RegisterForm()
 
 	if form.validate_on_submit():
-		new_user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+		hashed_password = generate_password_hash(form.password.data, method='sha256') # Hashes password before being passed to database
+		new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
 		db.session.add(new_user)
 		db.session.commit()
 		return("<h1>new user has been created</h1>")
@@ -63,7 +75,14 @@ def signup():
 	
 	return(render_template('signup.html', form=form))
 
+@app.route('/dashboard')
+@login_required
+def dashboard():
+	return(render_template('dashboard.html', name=current_user.username))
+
 if __name__ == '__main__':
 	app.run(debug=True)
 
-print ("REACHED END")
+
+
+
